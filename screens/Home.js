ec608ac18405c4605c93fb5620e55102e1cbaa7a
cristian.orozco <cristian.orozco@globant.com>
2022-02-03 13:09:01 -0500

    Navigation Bar/header

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,48 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native'
 
-import { COLORS, SIZES, FONTS } from '../constants'
+import { COLORS, SIZES, FONTS, icons } from '../constants'
 
 function Home() {
-  return (
-    <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>
-      <Text>HOme</Text>
-    </View>
-  )
+  const renderNavbar = () => {
+    return (
+      <View style={styles.navbar}>
+        <TouchableOpacity style={styles.navbarButtons} onPress={() => console.log('back button')}>
+          <Image source={icons.back_arrow} style={styles.navbarIcons} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ ...styles.navbarButtons, alignItems: 'flex-end' }}
+          onPress={() => console.log('more button')}
+        >
+          <Image source={icons.more} style={styles.navbarIcons} />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
+  return <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>{renderNavbar()}</View>
 }
+
+const styles = StyleSheet.create({
+  navbar: {
+    flexDirection: 'row',
+    height: 80,
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+    paddingHorizontal: SIZES.padding,
+    backgroundColor: COLORS.white,
+  },
+
+  navbarButtons: {
+    justifyContent: 'center',
+    width: 50,
+  },
+
+  navbarIcons: {
+    width: 30,
+    height: 30,
+    tintColor: COLORS.primary,
+  },
+})
 
 export default Home
