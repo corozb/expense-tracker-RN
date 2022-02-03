1645d2203118d220bd6b496643f779e57960d038
cristian.orozco <cristian.orozco@globant.com>
2022-02-03 12:24:23 -0500

    project septup

---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+import { COLORS, SIZES, FONTS } from '../constants'
+
+function Home() {
+  return (
+    <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>
+      <Text>HOme</Text>
+    </View>
+  )
+}
+
+export default Home
