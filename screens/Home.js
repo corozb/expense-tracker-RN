8ffd5b99db9395c3b186c656e3dffb1c55c39c7a
cristian.orozco <cristian.orozco@globant.com>
2022-02-03 14:23:30 -0500

    Header Section

--- screens/Home.js
+++ screens/Home.js
@@ -20,7 +20,34 @@ function Home() {
     )
   }
 
-  return <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>{renderNavbar()}</View>
+  const renderHeader = () => {
+    return (
+      <View style={{ paddingHorizontal: SIZES.padding, paddingVertical: SIZES.padding, backgroundColor: COLORS.white }}>
+        <View>
+          <Text style={{ color: COLORS.primary, ...FONTS.h2 }}>My Expenses</Text>
+          <Text style={{ color: COLORS.darkgray, ...FONTS.h3 }}>Summary</Text>
+        </View>
+
+        <View style={{ flexDirection: 'row', marginTop: SIZES.padding, alignItems: 'center' }}>
+          <View style={styles.imageContainer}>
+            <Image source={icons.calendar} style={{ width: 20, height: 20, tintColor: COLORS.lightBlue }} />
+          </View>
+
+          <View style={{ marginLeft: SIZES.padding }}>
+            <Text style={{ color: COLORS.primary, ...FONTS.h3 }}>21 Dec, 2020</Text>
+            <Text style={{ ...FONTS.body3, color: COLORS.darkgray }}>13% more than last month</Text>
+          </View>
+        </View>
+      </View>
+    )
+  }
+
+  return (
+    <View style={{ flex: 1, backgroundColor: COLORS.lightGray2 }}>
+      {renderNavbar()}
+      {renderHeader()}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -42,6 +69,15 @@ const styles = StyleSheet.create({
     width: 30,
     height: 30,
     tintColor: COLORS.primary,
+  },
+
+  imageContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: COLORS.lightGray,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
 
